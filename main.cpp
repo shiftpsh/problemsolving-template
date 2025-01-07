62eb67f88d7f0e7c66cc10d4aa2b5f6af33592d2
Suhyun Park <me@shiftpsh.com>
2025-01-07 14:01:20 +0900

    chore: initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+/* [💭] [💡] [🎈] */
+
+const int inf = 1e9;
+
+int main() {
+    cin.tie(nullptr), ios::sync_with_stdio(false);
+    
+#ifdef _SHIFTPSH
+    freopen("_run/in.txt", "r", stdin), freopen("_run/out.txt", "w", stdout);
+#endif
+
+    return 0;
+}
